8d64288f56fc7927dca6b662acabf9d9fac1688b
Ezekiel200483 <olopade.ezekiel1998@gmail.com>
2024-02-27 13:12:34 +0100

    modified:   src/components/chartbox/ChartBox.tsx

--- src/components/chartbox/ChartBox.tsx
+++ src/components/chartbox/ChartBox.tsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import "./chartBox.scss";
 import { Line, LineChart, ResponsiveContainer, Tooltip } from "recharts";
  // import React from "react";
+ // import { Link } from "react-router-dom";
 
 type Props = {
   color: string;

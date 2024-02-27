3ab0b84f086f34e3d44867c4e857c0cc95252e88
Ezekiel200483 <olopade.ezekiel1998@gmail.com>
2024-02-27 11:49:48 +0100

    ADMIN DASHBOARD

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,10 @@
+import "./footer.scss"
+const Footer = () => {
+  return (
+    <div className="navbar">
+      <span>Flutterwave</span>
+      <span>©Dev Dashboard</span>
+    </div>
+  );
+};
+export default Footer;
